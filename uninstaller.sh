f7e8a4281b2ff12707574de3313ebd186de3b986
hradec <hradec@gmail.com>
2023-03-29 18:57:02 -0700

    removed unused files

--- uninstaller.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm -rf ~/.WineApps/Adobe-Photoshop
-rm -rf ~/.local/share/icons/photoshop.png
-rm -rf  ~/.local/share/applications/photoshop.desktop
-
-zenity --info --text="Photoshop uninstalled."
